9cc425de2e008ab2562b3434d17dbeb9ae33e598
Anish Biswas <164672680+Anish-xii@users.noreply.github.com>
2026-05-29 10:03:02 +0100

    Add: subtree-of-a-binary-tree - submission-0

---
+++ Data Structures & Algorithms/subtree-of-a-binary-tree/submission-0.py
@@ -0,0 +1,15 @@
+# O(M * N) recursion DFS
+class Solution:
+    def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
+        
+        def sameTree(p, q):
+            if not p and not q: return True
+            if p and q and p.val == q.val: 
+                return sameTree(p.left, q.left) and sameTree(p.right, q.right)
+            else: return False
+
+        if not subRoot: return True
+        if not root: return False
+
+        if sameTree(root, subRoot): return True
+        return self.isSubtree(root.left, subRoot) or self.isSubtree(root.right, subRoot)
